d523fd3a98c9b4e01f856603b841d67dd0e1f86c
Yianis Karopoulos <j0nnys@users.noreply.github.com>
2016-10-24 23:17:17 +0300

    Add files via upload

---
+++ C-Sharp/ExerciseMobile/Battery.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mobile
+{
+    enum BatteryType
+    {
+        LithiumIon,
+        LithiumPolymer,
+        NickelCadmium,
+        NickelMetalHybride,
+        NewLithiumtechnology
+    }
+
+    class Battery
+    {
+        // private fields
+        private BatteryType type;
+        private int capacity;
+
+        // public properties
+        public BatteryType Type
+        {
+            get
+            {
+                return type;
+            }
+            private set
+            {
+                type = value;
+            }
+        }
+
+        public int Capacity
+        {
+            get
+            {
+                return capacity;
+            }
+            private set
+            {
+                if (value > 0 && value <= Int32.MaxValue)
+                {
+                    capacity = value;
+                }
+            }
+        }
+
+        // Constructor
+        public Battery(BatteryType type, int capacity)
+        {
+            Type = type;
+            Capacity = capacity;
+        }
+    }
+}
