067d3b147f83d50940545c6798cb4698354fe5b5
Yianis Karopoulos <j0nnys@users.noreply.github.com>
2016-10-24 23:19:22 +0300

    Add files via upload

---
+++ C-Sharp/ExerciseSubsequence/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Subsequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                List<int> list = new List<int>() { 5, 10, 10, 4, 4, 4, 2, 3, 6, 6, 6, 6 };
+
+                List<int> sequence = Utilities.Subsequence(list);
+                sequence.ForEach(x => Console.Write(x + " "));
+
+                List<int> sequence1 = list.Subsequence();
+                sequence1.ForEach(x => Console.Write(x + " "));
+
+                List<int> sequence2 = Utilities.SubLINQ(list);
+                sequence2.ForEach(x => Console.Write(x + " "));
+
+                Console.WriteLine();
+                Console.ReadKey();
+            
+
+        }
+    }
+}
+   
